af7fa716c78088766d937ec44aa94cd04cc7a401
JimCampagno <jimcampagno@gmail.com>
2015-04-02 21:29:50 -0400

    Coredata

---
+++ ToDoList/Subtask.h
@@ -0,0 +1,23 @@
+//
+//  Subtask.h
+//  ToDoList
+//
+//  Created by Jim Campagno on 4/2/15.
+//  Copyright (c) 2015 JimNick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class MainTask;
+
+@interface Subtask : NSManagedObject
+
+@property (nonatomic, retain) NSString * thingToBeDone;
+@property (nonatomic, retain) NSDate * startDate;
+@property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSDate * endDate;
+@property (nonatomic, retain) NSString * type;
+@property (nonatomic, retain) MainTask *mainTask;
+
+@end
